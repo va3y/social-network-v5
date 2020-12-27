f0126eb4d15c857d7aa52c8449b68b451b4fd4aa
valeriy <valerih333@gmail.com>
2020-12-27 14:41:18 +0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter } from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -13,6 +13,8 @@
   </div>
 </template>
 
+
+
 <script>
 import firebase from 'firebase/app'
 
